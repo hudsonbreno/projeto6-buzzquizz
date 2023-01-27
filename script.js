26c12d4ac46ba5bb42c64f1a6d4163622d44bece
Breno <brennoohudson@gmail.com>
2023-01-27 00:16:59 -0300

    Second screen create quizz

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Global Variables 
-const quizzesArea = document.querySelector('.quizzes-area-container')
+const quizzesArea = document.querySelector('.quizzes-area-container');
+let makequestion = document.querySelector(".question-screen-quizz");
 
 // Get all Quizzes from the server and display them on the homepage
 function getAllQuizzes() {
-    const data = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes')
+    const data = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
     data.then(response => {
         response = response.data      
 
@@ -26,11 +27,48 @@ function getAllQuizzes() {
     })
 }
 
-function hiddenAll(){
-    let all = document.querySelector(".all-quizzes-container");
-    all.classList.add("hidden");
+function hiddenquestion(quests){
+    for(i = 2; i <= quests; i++){
+        let esse = makequestion.querySelector(".pergunta"+i);
+        esse.classList.add("hidden");
+
+    }
+}
+
+function showquestion(e){
+    let pai = e.parentNode;
+    console.log(String(pai.classList[1]).substr(7));
+
+}
+
+function nextCreateQuest(){
     let create = document.querySelector(".screen-create-quizz");
     create.classList.add("hidden");
+    makequestion.classList.remove("hidden");
+    let quests = document.querySelector(".qtd-quest").value;
+    for(i = 1; i <= quests; i++) {
+        makequestion.innerHTML +=`
+                                <div class="question-header header-pergunta${i}">
+                                    <h1 class="question-h1">Pergunta ${i}</h1>
+                                    <ion-icon onclick="showquestion(this)" name="create-outline"></ion-icon>
+                                </div>
+                                <div class="question pergunta${i}">
+                                    <input type="text" class="title-question" placeholder="Título do seu quizz"/>
+                                    <input type="text" class="url-img-quizz" placeholder="Cor de fundo da pergunta"/>
+                                    <h1 class="question-h1">Pergunta correta</h1>
+                                    <input type="text" class="qtd-quest" placeholder="Resposta correta">
+                                    <input type="text" class="qtd-levels" placeholder="URL da imagem">
+                                    <h1 class="question-h1">Pergunta incorretas</h1>
+                                    <input type="text" class="qtd-quest" placeholder="Resposta incorreta 1">
+                                    <input type="text" class="qtd-levels" placeholder="URL da imagem 1">
+                                    <input type="text" class="qtd-quest" placeholder="Resposta incorreta 2">
+                                    <input type="text" class="qtd-levels" placeholder="URL da imagem 2">
+                                    <input type="text" class="qtd-quest" placeholder="Resposta incorreta 3">
+                                    <input type="text" class="qtd-levels" placeholder="URL da imagem 3">
+                                </div>    
+                                `;
+    }
+    hiddenquestion(quests);
 }
 
 function createQuizz(){
